1aee968eea5e538389c5b3e9798693fc57c85ac0
Kalu-Uche Rejoice <rejoicekaluuche@gmail.com>
2023-03-31 13:10:44 +0100

    first FYP commit

---
+++ assets/js/drop.js
@@ -0,0 +1,12 @@
+function generate_year_range(start, end) {
+    var years = "";
+    for (var year = start; year <= end; year++) {
+        years += "<option value='" + year + "'>" + year + "</option>";
+    }
+    return years;
+  }
+  let today = new Date();
+  let currentYear = today.getFullYear();
+  let selectYear = document.getElementById("Year");
+  let createYear = generate_year_range( 2010, currentYear );
+  selectYear.innerHTML = createYear;
