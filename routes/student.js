cb1ad6306a23a5d5e4f093140c514b1455b93abc
Kalu-Uche Rejoice <rejoicekaluuche@gmail.com>
2023-04-06 14:57:35 +0100

    EJS changes

--- routes/student.js
+++ routes/student.js
@@ -3,15 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/log', function(req, res, next) {
-    res.render('project monitoring module', { title: 'Express' });
+    res.render('project monitoring module');
 });
 router.get('/proposal', function(req, res, next) {
-    res.render('auth-login-basic', { title: 'Express' });
+    res.render('proposal module', { title: 'Express' });
 });
 router.get('/past-project', function(req, res, next) {
-    res.render('auth-login-basic', { title: 'Express' });
+    res.render('past FYP', { title: 'Express' });
+});
+router.get('/project-upload', function(req, res, next) {
+    res.render('project final upload module', { title: 'Express' });
 });
 router.get('/clearance', function(req, res, next) {
-    res.render('auth-login-basic', { title: 'Express' });
+    
+    res.render('cleared-student', { title: 'Express' });
   });
 module.exports = router;
